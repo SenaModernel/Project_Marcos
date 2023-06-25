67a3b21d6126693e72b5ee618421519361f08921
MauricioGuizzo <mauricioguizzo95@gmail.com>
2023-06-25 01:24:58 -0300

    Resolução de bugs nas coordenadas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from tkinter import simpledialog
+from funcoes import menu
 
 pygame.init()
 
@@ -16,10 +17,12 @@ fundo_game = pygame.image.load("bg.jpg")
 pygame.mixer.init()
 pygame.mixer.music.load("umPedido.mp3")
 pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.007)
 
 # Funções do game
 estrelas = {}
 linha = []
+nomes_estrelas = []
 
 # "FPS" do game
 clock = pygame.time.Clock()
@@ -29,8 +32,8 @@ running = True
 # Definir background
 game_tela.blit(fundo_game, (0, 0))
 
-linha_estrelas = 0
-primeiro_ponto = 0
+primeiro_ponto = True
+linha_estrelas = False
 posicao_linha_final = None
 
 while running:
@@ -41,38 +44,36 @@ while running:
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             item = simpledialog.askstring("Space", "Nome da Estrela:")
-            pygame.draw.circle(game_tela, ponto_branco, pos, 5)
+        
+            if not item:
+                item = "desconhecido" + str(pos)
+            else: 
+                item = item + str(pos)
+            
             estrelas[item] = pos
+            pygame.draw.circle(game_tela, ponto_branco, pos, 5)
 
-            if primeiro_ponto == 0:
-                if linha_estrelas == 0:
-                    posicao_linha = pos
-                    linha_estrelas += 1
-                    primeiro_ponto += 1
-
-            elif primeiro_ponto > 1 and linha_estrelas == 0:     
-                    posicao_linha = posicao_linha_final
-                    linha_estrelas += 1
-                    
-            elif linha_estrelas == 1:
-                posicao_linha_final = pos
-                pygame.draw.line(game_tela, ponto_branco, posicao_linha, posicao_linha_final, 2)
-                linha_estrelas -= 1
-
-            if posicao_linha_final is not None:
-                pygame.draw.line(game_tela, ponto_branco, posicao_linha_final, pos, 2)
-                posicao_linha_final = pos
-                
 
             for key, value in estrelas.items():
                 
                 fonte = pygame.font.Font(None, 30)
-                texto = fonte.render(item, True, ponto_branco)
-                game_tela.blit(texto, (pos[0] - 10, pos[1] + 10))
+                texto = fonte.render(key, True, ponto_branco)
+                game_tela.blit(texto, (value[0] - 10, value[1] + 10))
 
-            if item == None:
-                item = "desconhecido"+str(pos)
+            if primeiro_ponto:
+                if not linha_estrelas:
+                    posicao_linha = pos
+                    linha_estrelas = True
+                    primeiro_ponto = False
             
+            elif linha_estrelas:
+                posicao_linha_final = pos
+                pygame.draw.line(game_tela, ponto_branco, posicao_linha, posicao_linha_final, 2)
+                linha_estrelas = False
+                    
+            if posicao_linha_final is not None:
+                pygame.draw.line(game_tela, ponto_branco, posicao_linha_final, pos, 2)
+                posicao_linha_final = pos
 
 
     pygame.display.update()
